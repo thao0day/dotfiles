586a74514277397e0de2b9e4496e720a10e24495
thao.dang <thao.dang@hellohealthgroup.com>
2021-10-20 15:03:59 +0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+My macbook config
---
+++ hammerspoon/init.lua
@@ -0,0 +1,49 @@
+--- A closure function
+function open(name)
+    return function()
+        hs.application.launchOrFocus(name)
+        if name == 'Finder' then
+            hs.appfinder.appFromName(name):activate()
+        end
+    end
+end
+-- chrome with tab
+function chrome_active_tab_with_name(name)
+    return function()
+        hs.osascript.javascript([[
+            // below is javascript code
+            var chrome = Application('Google Chrome');
+            chrome.activate();
+            var wins = chrome.windows;
+
+            // loop tabs to find a web page with a title of <name>
+            function main() {
+                for (var i = 0; i < wins.length; i++) {
+                    var win = wins.at(i);
+                    var tabs = win.tabs;
+                    for (var j = 0; j < tabs.length; j++) {
+                    var tab = tabs.at(j);
+                    tab.title(); j;
+                    if (tab.title().indexOf(']] .. name .. [[') > -1) {
+                            win.activeTabIndex = j + 1;
+                            return;
+                        }
+                    }
+                }
+            }
+            main();
+            // end of javascript
+        ]])
+    end
+end
+
+--- quick open applications
+hs.hotkey.bind({"alt"}, "E", open("Finder"))
+hs.hotkey.bind({"alt"}, "1", open("Google Chrome"))
+hs.hotkey.bind({"alt"}, "2", open("Visual Studio Code"))
+hs.hotkey.bind({"alt"}, "3", open("iTerm"))
+hs.hotkey.bind({"alt"}, "4", open("Postman"))
+hs.hotkey.bind({"alt"}, "5", open("Sequel Ace"))
+
+--- Use
+hs.hotkey.bind({"alt"}, "Z", chrome_active_tab_with_name("Zalo Web"))
